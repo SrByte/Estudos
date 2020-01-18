1cc94e5e2b2b90e5ff32c7dd263c828a3cc721f8
Elias Albuquerque de Oliveira <elias_albuquerque@yahoo.com.br>
2020-01-18 16:05:28 -0300

    Atualização

--- EFcore/Mvc/Controllers/ProdutoController.cs
+++ EFcore/Mvc/Controllers/ProdutoController.cs
@@ -6,57 +6,50 @@ using Dominio.Entidades;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace Mvc.Controllers {
-    public class ProdutoController : Controller {
-        private ApplicationDbContext _contexto;
+namespace Mvc.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        private readonly ApplicationDbContext _contexto;
 
-        public ProdutoController (ApplicationDbContext contexto) {
+        public ProdutoController(ApplicationDbContext contexto)
+        {
             _contexto = contexto;
         }
 
-        public IActionResult Index () {
-            // var produtos = _contexto.Produtos.Include (c => c.Categoria).ToList (); Sem LazyLoad
-            var queryDeProdutos = _contexto.Produtos.Where (c => c.Categoria.PermiteEstoque);
-            if (!queryDeProdutos.Any ()) {
-                return View (new List<Produto> ());
-            }
+        public IActionResult Index()
+        {
+            var queryDeProduto = _contexto.Produtos
+                .Where(p => p.Ativo && p.Categoria.PermiteEstoque)
+                .OrderBy(p => p.Nome);
 
-            return View (queryDeProdutos);
+            if(!queryDeProduto.Any())
+                return View(new List<Produto>());
+            
+            return View(queryDeProduto.ToList());
         }
-
+        
         [HttpGet]
-        public IActionResult Salvar () {
-            ViewBag.Categorias = _contexto.Categorias.ToList ();
-            return View ();
+        public IActionResult Salvar()
+        {
+            ViewBag.Categorias = _contexto.Categorias.ToList();
+            return View();
         }
-
-        // public IActionResult Editar (int id) {
-
-        //     var produto = _contexto.Produtos.First (produto => produto.ID == id);
-        //     return View ("Salvar", produto);
-        // }
-        // public async Task<IActionResult> Deletar (int id) {
-
-        //     var produto = _contexto.Produtos.First (produto => produto.ID == id);
-        //     _contexto.Produtos.Remove (produto);
-        //     await _contexto.SaveChangesAsync ();
-
-        //     return RedirectToAction ("Index");
-        // }
 
         [HttpPost]
-        public async Task<IActionResult> Salvar (Produto modelo) {
-
-            if (modelo.ID == 0) {
-                _contexto.Produtos.Add (modelo);
-            } else {
-                var produto = _contexto.Produtos.First (c => c.ID == modelo.ID);
-                produto.Nome = modelo.Nome;
-            }
-
-            await _contexto.SaveChangesAsync ();
-            return RedirectToAction ("Index");
+        public async Task<IActionResult> Salvar(Produto modelo)
+        {
+            _contexto.Produtos.Add(modelo);
+            await _contexto.SaveChangesAsync();
+            return RedirectToAction("Index");
         }
+         public async Task<IActionResult> Deletar (int id) {
 
+            var produto = _contexto.Produtos.First (p => p.ID == id);
+            _contexto.Produtos.Remove(produto);
+            await _contexto.SaveChangesAsync();
+
+                        return RedirectToAction ("Index");
+        }
     }
 }
